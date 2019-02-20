0fe7a700987c19631cdfe56dad89c1be3a26da72
Brian Fife <BDFife@gmail.com>
2019-02-19 20:48:15 -0500

    Increase microstepping and reduce current.

--- SimpleEinsyRambo/Configuration.h
+++ SimpleEinsyRambo/Configuration.h
@@ -429,7 +429,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 160, 400, 96.5 } // Dr.Phil's settings
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320, 320, 400, 96.5 } // Dr.Phil's settings
 
 /**
  * Default Max Feed Rate (mm/s)
--- SimpleEinsyRambo/Configuration_adv.h
+++ SimpleEinsyRambo/Configuration_adv.h
@@ -1066,38 +1066,38 @@
   #define HOLD_MULTIPLIER    0.5  // Scales down the holding current from run current
   #define INTERPOLATE       true  // Interpolate X/Y/Z_MICROSTEPS to 256
 
-  #define X_CURRENT          750  // rms current in mA. Multiply by 1.41 for peak current.
-  #define X_MICROSTEPS        32  // 0..256
+  #define X_CURRENT          700  // rms current in mA. Multiply by 1.41 for peak current.
+  #define X_MICROSTEPS        64  // 0..256
 
-  #define Y_CURRENT          750
-  #define Y_MICROSTEPS        32
+  #define Y_CURRENT          700
+  #define Y_MICROSTEPS        64
 
-  #define Z_CURRENT          750
-  #define Z_MICROSTEPS        32
+  #define Z_CURRENT          700
+  #define Z_MICROSTEPS        64
 
-  #define X2_CURRENT         750
-  #define X2_MICROSTEPS       32
+  #define X2_CURRENT         700
+  #define X2_MICROSTEPS       64
 
-  #define Y2_CURRENT         750
-  #define Y2_MICROSTEPS       32
+  #define Y2_CURRENT         700
+  #define Y2_MICROSTEPS       64
 
-  #define Z2_CURRENT         750
-  #define Z2_MICROSTEPS       32
+  #define Z2_CURRENT         700
+  #define Z2_MICROSTEPS       64
 
-  #define E0_CURRENT         750
-  #define E0_MICROSTEPS       32
+  #define E0_CURRENT         700
+  #define E0_MICROSTEPS       64
 
-  #define E1_CURRENT         750
-  #define E1_MICROSTEPS       32
+  #define E1_CURRENT         700
+  #define E1_MICROSTEPS       64
 
-  #define E2_CURRENT         750
-  #define E2_MICROSTEPS       32
+  #define E2_CURRENT         700
+  #define E2_MICROSTEPS       64
 
-  #define E3_CURRENT         750
-  #define E3_MICROSTEPS       32
+  #define E3_CURRENT         700
+  #define E3_MICROSTEPS       64
 
-  #define E4_CURRENT         750
-  #define E4_MICROSTEPS       32
+  #define E4_CURRENT         700
+  #define E4_MICROSTEPS       64
 
   /**
    * Use software SPI for TMC2130.
