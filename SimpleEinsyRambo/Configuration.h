e0f8e1aca53cd6abf33319b0a7951fb3886818c9
Brian Fife <BDFife@gmail.com>
2019-02-19 21:17:41 -0500

    Calibrate Extruder.

--- SimpleEinsyRambo/Configuration.h
+++ SimpleEinsyRambo/Configuration.h
@@ -429,7 +429,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 640, 640, 3200, 96.5 } // Dr.Phil's settings
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 640, 640, 3200, 772 } 
 
 /**
  * Default Max Feed Rate (mm/s)
