260fab851fe63c88fc0272313d7fa4ccc176df51
Brian Fife <BDFife@gmail.com>
2019-02-21 18:20:13 -0500

    Restore 'safe extrude' mode.

--- SimpleEinsyRambo/Configuration.h
+++ SimpleEinsyRambo/Configuration.h
@@ -281,8 +281,8 @@
  * *** IT IS HIGHLY RECOMMENDED TO LEAVE THIS OPTION ENABLED! ***
  */
 
-//#define PREVENT_COLD_EXTRUSION
-//#define EXTRUDE_MINTEMP 170
+#define PREVENT_COLD_EXTRUSION
+#define EXTRUDE_MINTEMP 170
 
 /**
  * Prevent a single extrusion longer than EXTRUDE_MAXLENGTH.
