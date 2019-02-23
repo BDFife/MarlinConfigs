f7da9762552a40b7f05253f4a2d1bbb77f7f3387
Brian Fife <BDFife@gmail.com>
2019-02-23 05:31:18 -0500

    Disable STEALTHCHOP for now.

--- SimpleEinsyRambo/Configuration_adv.h
+++ SimpleEinsyRambo/Configuration_adv.h
@@ -1113,7 +1113,7 @@
    * Use Trinamic's ultra quiet stepping mode.
    * When disabled, Marlin will use spreadCycle stepping mode.
    */
-  #define STEALTHCHOP
+  // #define STEALTHCHOP
 
   /**
    * Monitor Trinamic TMC2130 and TMC2208 drivers for error conditions,
