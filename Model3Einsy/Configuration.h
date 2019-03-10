40967c2334c6b951062f224369b2bd665cca4701
Brian Fife <BDFife@gmail.com>
2019-03-10 08:25:08 -0400

    Change z ratio to account for geared stepper.

--- Model3Einsy/Configuration.h
+++ Model3Einsy/Configuration.h
@@ -429,7 +429,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 96.5 } 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 96.5 } 
 
 /**
  * Default Max Feed Rate (mm/s)
