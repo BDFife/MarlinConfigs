4ab06321889e4e84ed03dfd0d639db5a5698fa6c
Brian Fife <BDFife@gmail.com>
2019-03-10 06:35:38 -0400

    Update fan pin for E0.

--- Model3Einsy/Configuration_adv.h
+++ Model3Einsy/Configuration_adv.h
@@ -199,7 +199,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN 0 // disable FAN_PIN in pins_RAMBO.h
+#define E0_AUTO_FAN_PIN 8 // disable FAN_PIN in pins_RAMBO.h
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
